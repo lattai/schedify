11f57b8bb51ed72b4a034f145971f687f7b30a26
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-27 23:02:56 -0700

    main.py and app.yaml starter

--- main.py
+++ main.py
@@ -3,3 +3,29 @@ import os
 import jinja2
 
 from models import Meme
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', HomeHandler),
+    ('/schedule_page', ScheduleHandler),
+    ('/new_event_page', NewEventHandler),
+    ('/connections_page', ConnectionsHandler)
+
+], debug=True)
