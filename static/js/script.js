81490a875fda6cee3f3d89e0a467f9de39c83bba
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-30 13:04:01 -0700

    sched page buttons :)

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,38 @@
-const compare = document.querySelector('#compare1');//any css selector
+console.log("clicked!")
 
-var fullSched = document.querySelector("#your-month")
+const compareClass = document.querySelector('.compare');//compare div
+compareClass.style.visibility  = "hidden" //"visable"
 
-fullSched.addEventListener
-compare.addEventListener("click",(e))=>{
-  fullSched.width = "0"
-  fullSched.height = "0"
-}
+
+const fullSched = document.querySelector("#your-month")
+
+const compare1 = document.querySelector("#compare1")
+const compare2 = document.querySelector("#compare1")
+const compare3 = document.querySelector("#compare1")
+const resetview = document.querySelector("#resetScheduleView")
+
+compare1.addEventListener("click",e =>{
+  console.log("clicked!")
+  fullSched.style.height = 0
+  fullSched.style.width = 0
+  compareClass.style.visibility = "visible"
+})
+compare2.addEventListener("click",e =>{
+  console.log("clicked!")
+  fullSched.style.height = 0
+  fullSched.style.width = 0
+  compareClass.style.visibility = "visible"
+})
+compare3.addEventListener("click",e =>{
+  console.log("clicked!")
+  fullSched.style.height = 0
+  fullSched.style.width = 0
+  compareClass.style.visibility = "visible"
+})
+resetview.addEventListener("click",e =>{
+  console.log("clicked!")
+  fullSched.style.height = 600
+  fullSched.style.width = 1014
+  fullSched.style.visibility = "visible"
+  compareClass.style.visibility = "hidden"
+})
