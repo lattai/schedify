2e3d023270c83a6ec242662578518b8496d1fcc9
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-30 12:24:02 -0700

    cal embedding

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+const compare = document.querySelector('#compare1');//any css selector
+
+var fullSched = document.querySelector("#your-month")
+
+fullSched.addEventListener
+compare.addEventListener("click",(e))=>{
+  fullSched.width = "0"
+  fullSched.height = "0"
+}
